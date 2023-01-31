952408d93a2437cbca690a0003c2c115f65589c9
Zapodask <joaopedroemerim@gmail.com>
2023-01-31 15:44:48 -0300

    finish

--- index.js
+++ index.js
@@ -18,13 +18,37 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date?", function (req, res) {
+  let inputDate = req.params.date
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  if (!inputDate) {
+    const today = new Date()
+
+    res.json({
+      unix: today.getTime(),
+      utc: today.toUTCString()
+    })
+
+    return
+  }
+
+  if (!isNaN(Number(inputDate))) {
+    inputDate = Number(inputDate)
+  }
+
+  const d = new Date(inputDate);
+
+  if (!isNaN(d.getTime())) {
+    res.json({
+      unix: d.getTime(),
+      utc: d.toUTCString()
+    })
+  } else {
+    res.json({
+      error: 'Invalid Date'
+    })
+  }
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
